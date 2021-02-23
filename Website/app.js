46a679e8c52775c36226582edce7080fc94fb7e7
Justin Klein <JustinKln51@gmail.com>
2021-02-23 09:37:29 +0100

    added js code

---
+++ Website/app.js
@@ -0,0 +1,13 @@
+const input = document.getElementById("userInput");
+const button = document.getElementById("buttonClick");
+button.addEventListener("click", callBack);
+
+function callBack(){
+    const value = input.value;
+
+    fetch('https://pokeapi.co/api/v2/pokemon${value}')
+  .then(response => response.json())
+  .then(data => console.log(data));
+    console.log(1);
+}
+callBack();
